9439f210142c9f565b27affba5b04a8211b1d149
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-14 18:18:55 +0100

    tests passing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,32 @@
 // Test away!
+
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import Display from './Display';
+
+let display;
+
+afterEach(rtl.cleanup)
+
+beforeEach(() => {
+    display = rtl.render(<Display />);
+  });
+
+describe('Display component', () => {
+
+    test('should match snapshot', () => {
+        expect(display).toMatchSnapshot();
+    })
+
+    test('displays Closed if the `closed` prop is `true` and Open if otherwise', () => {
+        const displayClosed = rtl.render(<Display closed={true}/>);
+        expect(displayClosed.queryByText(/Closed/i)).toBeInTheDocument();
+      });
+
+    // test('displays Closed if the `closed` prop is `true` and Open if otherwise', () => {
+    // const displayLocked = rtl.render(<Display locked={true}/>);
+    // expect(displayLocked.queryByText(/Locked/i)).toBeInTheDocument();
+    // });
+
+})
