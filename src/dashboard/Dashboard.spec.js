7ce6051844b82399334ddac65670a0c65cd15f48
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-14 16:53:30 +0100

    snapshot tests for dashboard and controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,32 @@
 // Test away
+
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+let dashboard;
+
+afterEach(rtl.cleanup)
+
+beforeEach(() => {
+    dashboard = rtl.render(<Dashboard />);
+  });
+
+  describe('App component', () => {
+
+    test('should match snapshot', () => {
+        expect(dashboard).toMatchSnapshot();
+    })
+
+    test('default is unlocked', () => {
+        
+        expect(dashboard.queryByText(/Unlocked/i)).toBeInTheDocument();
+    });
+
+    test('default is open', () => {
+        
+        expect(dashboard.queryByText(/Open/i)).toBeInTheDocument();
+    });
+
+  })
